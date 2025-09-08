f58091e03f02cbc6d84e918c80524e5d8bb767b0
botcott <albertahmathanov18@gmail.com>
2025-09-08 18:07:44 +0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+import logging
+from discord.ext import commands
+
+# .env
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.members = True 
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# events
+@bot.event
+async def on_ready():
+    logging.info(f"logged in as {bot.user}")
+
+# to load cog use this for example: bot.load_extension("cogs.judges-mention-cog.__init__")
+# to load cog use this for example:  bot.load_extension("cogs.judges-appeals-cog.__init__")
+bot.run(os.getenv("token"))
